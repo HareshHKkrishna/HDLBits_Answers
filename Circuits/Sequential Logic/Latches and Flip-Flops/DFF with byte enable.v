3c7d64acaa33594ca1f854a5ea22a042a1fd343b
Haresh Krishna GS <117556983+HareshHKkrishna@users.noreply.github.com>
2025-12-31 09:17:25 +0530

    Update CASE Statement

--- Circuits/Sequential Logic/Latches and Flip-Flops/DFF with byte enable.v
+++ Circuits/Sequential Logic/Latches and Flip-Flops/DFF with byte enable.v
@@ -40,3 +40,32 @@ module top_module (
     end
 
 endmodule
+
+// CASE STATEMENT :
+/*
+module top_module (
+    input clk,
+    input resetn,
+    input [1:0] byteena,
+    input [15:0] d,
+    output [15:0] q
+);
+    always@(posedge clk)begin
+        if(~resetn)begin
+            case(byteena)
+                2'b00 : q<=q;
+                2'b01 : q[7:0]<=8'b0;
+                2'b10 : q[15:8]<=8'b0;
+                2'b11 : q[15:0]<=16'b0;
+            endcase
+        end
+        else begin
+        	case(byteena)
+                2'b00 : q<=q;
+                2'b01 : q[7:0]<=d[7:0];
+            	2'b10 : q[15:8]<=d[15:8];
+            	2'b11 : q[15:0]<=d[15:0];
+            endcase	
+        end
+    end
+endmodule */
